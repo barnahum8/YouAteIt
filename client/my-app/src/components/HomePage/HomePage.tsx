ce452a32d40643ad78679d4bb55d4a7cee0c4366
BARNA-LAP-HZ\barna <bar.nahum8@gmail.com>
2020-12-14 09:51:21 +0200

    adding react hooks to home page

--- client/my-app/src/components/HomePage/HomePage.tsx
+++ client/my-app/src/components/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AppBar } from '@material-ui/core';
 import './HomePage.css';
@@ -8,43 +9,27 @@ import Swal from 'sweetalert2';
 
 const CLIENT_ID = '213563774258-u65snpotb10nmhh5lniou72ph518auv4.apps.googleusercontent.com';
 
-interface MyProps {
-}
+function HomePage(props) {;
 
-interface MyState {
-    isLogined: boolean,
-    userEmail: string
-}
-
-export class HomePage extends React.Component<MyProps,MyState> {
-  constructor(props: MyProps) {
-    super(props);
-
-    this.state = {
-      isLogined: false,
-      userEmail: ''
-    };
-  }
+    const [isLogined, setIsLogined] = useState<boolean>(false);
+    const [userEmail, setUserEmail] = useState<string>('');
 
   // login to google account
-  login =  (response:any) => {
-    if(response.vt && response.vt.bu){
-      this.setState(state => ({
-        isLogined: true,
-        userEmail: response.vt.bu
-      }));
+  const login =  (response:any) => {
+    console.log(response.xt);
+    if(response.xt && response.xt.du){
+      setIsLogined(true);
+      setUserEmail(response.xt.du);
     }
   }
 
   // logout of google account
-  logout = () => {
-    this.setState(state => ({
-      isLogined: false,
-      userEmail: ''
-    }));
+  const logout = () => {
+    setIsLogined(false);
+    setUserEmail('');
   }
 
-  handleLogoutFailure = () => {
+  const handleLogoutFailure = () => {
     Swal.fire({
       title: '!שגיאה',
       text: '.המערכת לא הצליחה להתחבר',
@@ -53,39 +38,36 @@ export class HomePage extends React.Component<MyProps,MyState> {
     })
   }
 
-
-  public render() {
-    return (
-      <div>
-          <AppBar position="static">
-            <div>
-              <h2 className="title">אכלת אותה</h2>
-              <img alt="logo" className="logo" src="logo.png" width='80' height="60"></img>
-              <div className="username" hidden={!this.state.isLogined}>
-                <GoogleLogout
-                  clientId={ CLIENT_ID }
-                  buttonText='התנתק'
-                  onLogoutSuccess={ this.logout }
-                  onFailure={ this.handleLogoutFailure }
-                >
-                </GoogleLogout>
-                <p className="hello">{this.state.userEmail} שלום</p>
-            </div>
-            </div>
-          </AppBar>
-          { this.state.isLogined ?
-            <FormComp userEmail={this.state.userEmail}></FormComp>: <div className="comp">
-                <h2>...ברוכים הבאים לאכלת אותה! בואו נתחבר ומיד נתחיל</h2>
-                <GoogleLogin
+  return (
+    <div>
+        <AppBar position="static">
+          <div>
+            <h2 className="title">אכלת אותה</h2>
+            <img alt="logo" className="logo" src="logo.png" width='80' height="60"></img>
+            <div className="username" hidden={!isLogined}>
+              <GoogleLogout
                 clientId={ CLIENT_ID }
-                buttonText='sign in with Google'
-                onSuccess={ this.login }
-                cookiePolicy={ 'single_host_origin' }
-                responseType='code,token'/>
-              </div>}
-      </div>
-    );
-  }
+                buttonText='התנתק'
+                onLogoutSuccess={ logout }
+                onFailure={ handleLogoutFailure }
+              >
+              </GoogleLogout>
+              <p className="hello">{userEmail} שלום</p>
+          </div>
+          </div>
+        </AppBar>
+        { isLogined ?
+          <FormComp userEmail={userEmail}></FormComp>: <div className="comp">
+              <h2>...ברוכים הבאים לאכלת אותה! בואו נתחבר ומיד נתחיל</h2>
+              <GoogleLogin
+              clientId={ CLIENT_ID }
+              buttonText='sign in with Google'
+              onSuccess={ login }
+              cookiePolicy={ 'single_host_origin' }
+              responseType='code,token'/>
+            </div>}
+    </div>
+  );
 }
 
 export default HomePage;
