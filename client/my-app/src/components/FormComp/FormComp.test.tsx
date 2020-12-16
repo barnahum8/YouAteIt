4e666a4559cd6afdfce8095af0c9be18dc397204
BARNA-LAP-HZ\barna <bar.nahum8@gmail.com>
2020-12-16 14:24:38 +0200

    adding and learning enzyme + jest (doesnt support react 17)

--- client/my-app/src/components/FormComp/FormComp.test.tsx
+++ client/my-app/src/components/FormComp/FormComp.test.tsx
@@ -1,65 +1,70 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow, mount, render } from 'enzyme';
 import FormComp from './FormComp';
+import {Tabs,Tab, Box, Typography} from '@material-ui/core';
 
-const formFunctions = new FormComp({userEmail:''});
+// const formFunctions = FormComp;
 
-test('test namesValidation 1', () => {
-  const namesVal = formFunctions.namesValidation("123");
-  expect(namesVal).toBe(true);
-});
+//describe('A suite', function() {
+    
+// it('test namesValidation 1', () => {
+//   const namesVal = formFunctions.namesValidation("123");
+//   expect(namesVal).toBe(true);
+// });
 
-test('test namesValidation 2', () => {
-    const namesVal = formFunctions.namesValidation("aaa");
-    expect(namesVal).toBe(false);
-});
+// it('test namesValidation 2', () => {
+//     const namesVal = formFunctions.namesValidation("aaa");
+//     expect(namesVal).toBe(false);
+// });
 
-test('test namesValidation 3', () => {
-    const namesVal = formFunctions.namesValidation("אגא");
-    expect(namesVal).toBe(false);
-});
+// it('test namesValidation 3', () => {
+//     const namesVal = formFunctions.namesValidation("אגא");
+//     expect(namesVal).toBe(false);
+// });
 
-test('test namesValidation 4', () => {
-    const namesVal = formFunctions.namesValidation("דגכדגכsdfsdf");
-    expect(namesVal).toBe(false);
-});
+// it('test namesValidation 4', () => {
+//     const namesVal = formFunctions.namesValidation("דגכדגכsdfsdf");
+//     expect(namesVal).toBe(false);
+// });
 
-test('test namesValidation 5', () => {
-    const namesVal = formFunctions.namesValidation("asd12");
-    expect(namesVal).toBe(true);
-});
+// it('test namesValidation 5', () => {
+//     const namesVal = formFunctions.namesValidation("asd12");
+//     expect(namesVal).toBe(true);
+// });
 
-test('test dateValidation 1', () => {
-    const namesVal = formFunctions.dateValidation("11/12/2030");
-    expect(namesVal).toBe(true);
-});
+// it('test dateValidation 1', () => {
+//     const namesVal = formFunctions.dateValidation("11/12/2030");
+//     expect(namesVal).toBe(true);
+// });
 
-test('test dateValidation 2', () => {
-    const namesVal = formFunctions.dateValidation("11/12/1995");
-    expect(namesVal).toBe(false);
-});
+// it('test dateValidation 2', () => {
+//     const namesVal = formFunctions.dateValidation("11/12/1995");
+//     expect(namesVal).toBe(false);
+// });
 
-test('test idValidation 1', () => {
-    const namesVal = formFunctions.idValidation("207025222");
-    expect(namesVal).toBe(false);
-});
+// it('test idValidation 1', () => {
+//     const namesVal = formFunctions.idValidation("207025222");
+//     expect(namesVal).toBe(false);
+// });
 
-test('test idValidation 2', () => {
-    const namesVal = formFunctions.idValidation("207025223");
-    expect(namesVal).toBe(true);
-});
+// it('test idValidation 2', () => {
+//     const namesVal = formFunctions.idValidation("207025223");
+//     expect(namesVal).toBe(true);
+// });
 
-test('test phoneValidation 1', () => {
-    const namesVal = formFunctions.phoneValidation("456");
-    expect(namesVal).toBe(true);
-});
+// it('test phoneValidation 1', () => {
+//     const namesVal = formFunctions.phoneValidation("456");
+//     expect(namesVal).toBe(true);
+// });
 
-test('test phoneValidation 2', () => {
-    const namesVal = formFunctions.phoneValidation("05467675");
-    expect(namesVal).toBe(true);
-});
+// it('test phoneValidation 2', () => {
+//     const namesVal = formFunctions.phoneValidation("05467675");
+//     expect(namesVal).toBe(true);
+// });
 
-test('test phoneValidation 3', () => {
-    const namesVal = formFunctions.phoneValidation("0542255467");
-    expect(namesVal).toBe(false);
-});
+// it('test phoneValidation 3', () => {
+//     const namesVal = formFunctions.phoneValidation("0542255467");
+//     expect(namesVal).toBe(false);
+// });
+//   });
+
