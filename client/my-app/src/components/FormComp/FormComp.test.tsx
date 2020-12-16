7046a991d70371575c88ee27195e66da8ebb884a
BARNA-LAP-HZ\barna <bar.nahum8@gmail.com>
2020-12-16 14:29:48 +0200

    testing names

--- client/my-app/src/components/FormComp/FormComp.test.tsx
+++ client/my-app/src/components/FormComp/FormComp.test.tsx
@@ -7,64 +7,64 @@ import {Tabs,Tab, Box, Typography} from '@material-ui/core';
 
 //describe('A suite', function() {
     
-// it('test namesValidation 1', () => {
+// it('validate name with numbers', () => {
 //   const namesVal = formFunctions.namesValidation("123");
-//   expect(namesVal).toBe(true);
+//   expect(namesVal).toBe(false);
 // });
 
-// it('test namesValidation 2', () => {
+// it('validate name with english letters', () => {
 //     const namesVal = formFunctions.namesValidation("aaa");
-//     expect(namesVal).toBe(false);
+//     expect(namesVal).toBe(true);
 // });
 
-// it('test namesValidation 3', () => {
+// it('validate name with hebrew letters', () => {
 //     const namesVal = formFunctions.namesValidation("אגא");
-//     expect(namesVal).toBe(false);
+//     expect(namesVal).toBe(true);
 // });
 
-// it('test namesValidation 4', () => {
+// it('validate name with english and hebrew letters', () => {
 //     const namesVal = formFunctions.namesValidation("דגכדגכsdfsdf");
-//     expect(namesVal).toBe(false);
+//     expect(namesVal).toBe(true);
 // });
 
-// it('test namesValidation 5', () => {
+// it('validate name with english letters and numbers', () => {
 //     const namesVal = formFunctions.namesValidation("asd12");
-//     expect(namesVal).toBe(true);
+//     expect(namesVal).toBe(false);
 // });
 
-// it('test dateValidation 1', () => {
+// it('validate future date', () => {
 //     const namesVal = formFunctions.dateValidation("11/12/2030");
-//     expect(namesVal).toBe(true);
+//     expect(namesVal).toBe(false);
 // });
 
-// it('test dateValidation 2', () => {
+// it('validate valid date', () => {
 //     const namesVal = formFunctions.dateValidation("11/12/1995");
-//     expect(namesVal).toBe(false);
+//     expect(namesVal).toBe(true);
 // });
 
-// it('test idValidation 1', () => {
+// it('validate real id', () => {
 //     const namesVal = formFunctions.idValidation("207025222");
-//     expect(namesVal).toBe(false);
+//     expect(namesVal).toBe(true);
 // });
 
-// it('test idValidation 2', () => {
+// it('validate fake id', () => {
 //     const namesVal = formFunctions.idValidation("207025223");
-//     expect(namesVal).toBe(true);
-// });
-
-// it('test phoneValidation 1', () => {
-//     const namesVal = formFunctions.phoneValidation("456");
-//     expect(namesVal).toBe(true);
-// });
-
-// it('test phoneValidation 2', () => {
-//     const namesVal = formFunctions.phoneValidation("05467675");
-//     expect(namesVal).toBe(true);
-// });
-
-// it('test phoneValidation 3', () => {
-//     const namesVal = formFunctions.phoneValidation("0542255467");
 //     expect(namesVal).toBe(false);
+// });
+
+// it('validate short phone number', () => {
+//     const namesVal = formFunctions.phoneValidation("456");
+//     expect(namesVal).toBe(false);
+// });
+
+// it('validate short phone number but with 0', () => {
+//     const namesVal = formFunctions.phoneValidation("05467675");
+//     expect(namesVal).toBe(false);
+// });
+
+// it('validate valid phone number', () => {
+//     const namesVal = formFunctions.phoneValidation("0542255467");
+//     expect(namesVal).toBe(true);
 // });
 //   });
 
